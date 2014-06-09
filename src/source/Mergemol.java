affe0b171c2be891fecf22acb02cb648c0e5667b
mirkos <mirkos@LG-AIO>
2014-06-09 16:06:37 -0300

    Criação da classe para merge entre diferentes moléculas

---
+++ src/source/Mergemol.java
@@ -0,0 +1,19 @@
+package source;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+
+public class Mergemol {
+	
+	Principal p = new Principal();
+	
+	LinkedList<Estrutura> molecula1 = new LinkedList<Estrutura>();
+	LinkedList<Estrutura> molecula2 = new LinkedList<Estrutura>();
+	BigDecimal disBtwMol = new BigDecimal("0.00");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
